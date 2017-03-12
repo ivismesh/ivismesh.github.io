ef5d523c1a9656a487ff8c95b79523a61cc56648
jonatancoster <jonatan.coster@gmail.com>
2017-03-12 23:47:44 +0100

    Update visualization.js

--- arbor/visualization.js
+++ arbor/visualization.js
@@ -143,7 +143,7 @@ function update(source) {
       links = tree.links(nodes);
 
   // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 320; });
+  nodes.forEach(function(d) { d.y = d.depth * 380; });
 
   // Update the nodes…
   var node = svg.selectAll("g.node")
