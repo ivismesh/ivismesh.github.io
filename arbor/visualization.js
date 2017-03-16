3fd0e2d6d9cea3064ea74db92b277f8d53402007
jonatancoster <jonatan.coster@gmail.com>
2017-03-16 20:46:59 +0100

    Add files via upload

--- arbor/visualization.js
+++ arbor/visualization.js
@@ -298,8 +298,7 @@ function update(source) {
 		.duration(duration)
 		.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
-	nodeUpdate.select("circle")
-		.attr("r", function(d) {if(d.name === searchText) return 9; else return 4.5})
+	
 
 	nodeUpdate.select("text")
 		.style("fill-opacity", 1);
@@ -408,7 +407,7 @@ function search(string) {
 			}
 		})
 		.selectAll("circle")
-		.attr("r", 48)
+		.attr("r", 18)
 		.style("filter", function(d) {return "url(#glow)"});
 }
 
