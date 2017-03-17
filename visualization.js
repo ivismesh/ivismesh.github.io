5e5b725a8ee32a9ba03d7fa92d74eb658b369084
jonatancoster <jonatan.coster@gmail.com>
2017-03-17 12:24:11 +0100

    Add files via upload

--- visualization.js
+++ visualization.js
@@ -1,13 +1,16 @@
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 960 - margin.right - margin.left,
-    height = 800 - margin.top - margin.bottom;
+var margin = {top: 200, right: 120, bottom: 20, left: 120},
+    width = $("#visualization").width() - margin.right - margin.left,
+    height = $("#visualization").height() - margin.top - margin.bottom;
 
 var i = 0,
-    duration = 750,
-    root;
+  duration = 750,
+  root,
+	descToPaths,
+	searchText;
 
 var tree = d3.layout.tree()
-    .size([height, width]);
+	.nodeSize([25, 25]);
+    //.size([height, width]);
 
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
@@ -25,12 +28,54 @@ var svg = d3.select("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-function zoomed(){
+function zoomed() {
 	var tx = Math.min(150, d3.event.translate[0]),
 		//ty = Math.min(0, d3.event.translate[1]);
 		ty = d3.event.translate[1];
-	svg.attr("transform", "translate(" + [tx, ty] + ")scale(" + d3.event.scale + ")");
+	svg.attr("transform", "translate(" + [tx, ty] + ") scale(" + d3.event.scale + ")");
 }
+
+//Container for the gradients
+var defs = svg.append("defs");
+
+//Filter for the outside glow
+var filter = defs.append("filter")
+	.attr("x", "-100%")
+	.attr("y", "-100%")
+	.attr("width", "300%")
+	.attr("height", "300%")
+	.attr("id","glow");
+
+filter.append("feFlood")
+	.attr("flood-color", "#FF0000")
+	.attr("flood-opacity","1")
+	.attr("result","flood");
+
+filter.append("feComposite")
+	.attr("in","flood")
+	.attr("in2","SourceGraphic")
+	.attr("operator","in")
+	.attr("result","mask");
+
+filter.append("feMorphology")
+	.attr("in","mask")
+	.attr("radius","2")
+	.attr("operator","dilate")
+	.attr("result","dilated");
+
+filter.append("feGaussianBlur")
+	.attr("in","dilated")
+	.attr("stdDeviation","3")
+	.attr("result","blurred");
+
+var feMerge = filter.append("feMerge");
+
+feMerge.append("feMergeNode")
+	.attr("in","blurred");
+
+feMerge.append("feMergeNode")
+	.attr("in","SourceGraphic");
+
 
 
 
@@ -41,25 +86,129 @@ d3.json("data.json", function(error, data) {
   root.x0 = height / 2;
   root.y0 = 0;
 
-  function collapse(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapse);
-      d.children = null;
-    }
-  }
+  console.log(root);
 
-  root.children.forEach(collapse);
-  update(root);
-  
-  addresses = [];
+  d3.json("descNodes.json", function(error, data) {
+		if (error) throw error;
+
+		descToPaths = data;
+
+    searchText = decodeURIComponent(window.location.href.split("?searchtext=")[1]).replace(/\+/, ' ');
+
+    root.children.forEach(collapse);
+    if(searchText.length > 0) {
+      search(searchText);
+    }
+    else update(root);
+
+    // search form button onClick
+  	$("#searchButton").click(function(e) {
+  		e.preventDefault();
+  		search($("#searchText").val());
+  	});
+  	// search form onEnter
+  	$("#searchText").bind('keydown', e => enterKey(e));
+  	function enterKey(e) {
+  		if(e.keyCode == 13) {
+  			e.preventDefault();
+  			search($("#searchText").val());
+  		}
+  	}
+	});
+
+  $('#searchText').focus();
+
 });
 
 
 
 
 
+var colors = {
+	"Anatomy": "#C189C4",
+	"Organisms": "#C8457F",
+	"Diseases": "#795548",
+	"Chemicals and Drugs": "#FFA500",
+	"Analytical, Diagnostic and Therapeutic Techniques, and Equipment": "#A63603",
+	"Psychiatry and Psychology": "#E6550D",
+	"Phenomena and Processes": "#FD8D3C",
+	"Disciplines and Occupations": "#FDBE85",
+	"Anthropology, Education, Sociology, and Social Phenomena": "#006D2C",
+	"Technology, Industry, and Agriculture": "#31A354",
+	"Humanities": "#74C476",
+	"Information Science": "#BAE4B3",
+	"Named Groups": "#08519C",
+	"Health Care": "#3182BD",
+	"Publication Characteristics": "#6BAED6",
+	"Geographicals": "#BDD7E7"
+}
+
+
+
+
 d3.select(self.frameElement).style("height", "800px");
+
+
+function collapse(d) {
+  //console.log("collapsing: " + d.data.address);
+	if(d.children) {
+		if(!d._children) d._children = d.children;
+		else {
+			for(var child in d.children) {
+				d._children.push(d.children[child]);
+			}
+		}
+		if(d._children.length == 0) d._children = null;
+		d.children = null;
+		for(var child in d._children) {
+			this.collapse(d._children[child]);
+		}
+	}
+}
+
+/*
+ * Expands all paths to the searched nodes.
+ */
+ function expand(d, addr) {
+ 	/*console.log("growing: " + d.data.address + ' to ' + addr);
+ 	console.log(d);*/
+
+ 	// if this d has address == addr
+ 	if(d.address == addr) {
+ 		if(d._children) {
+ 			if(!d.children) d.children = d._children;
+ 			else {
+ 				for(var child in d._children) {
+ 					d.children.push(d._children[child]);
+ 				}
+ 			}
+ 		}
+ 		d._children = null;
+ 		return;
+ 	}
+
+ 	// else if thid d has address shorter than addr
+ 	else if(addr.length > d.address.length) {
+ 		let subPath = addr.slice(0, d.address.length);
+ 		if(d._children) {
+ 			if(!d.children) d.children = [];
+ 			for(var child in d._children) {
+ 				if(d._children[child].address == addr.slice(0, d._children[child].address.length)) {
+ 					d.children.push(d._children.splice(child, 1)[0]);
+ 					break;
+ 				}
+ 			}
+ 			if(d._children.length == 0) d._children = null;
+ 		}
+ 		if(d.children) {
+ 			for(var child in d.children) {
+ 				if(d.children[child].address == addr.slice(0, d.children[child].address.length)) {
+ 					expand(d.children[child], addr);
+ 				}
+ 			}
+ 		}
+ 	}
+}
 
 function update(source) {
 
@@ -68,7 +217,9 @@ function update(source) {
       links = tree.links(nodes);
 
   // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
+  nodes.forEach(function(d) {
+    d.y = d.depth * 500;
+  });
 
   // Update the nodes…
   var node = svg.selectAll("g.node")
@@ -80,18 +231,56 @@ function update(source) {
       .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
       .on("click", click);
 
-  nodeEnter.append("circle")
+
+
+	//Use circles for all but first level.
+  nodeEnter.filter(function(d) {
+	  if(d.depth === 1) return false;
+	  else return true;
+	})
+	.append("circle")
       .attr("r", 1e-6)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; })
+      .style("fill", d => d._children ? colorMe(d.address) : "#FFF")
+      .style("stroke", d => colorMe(d.address))
 	  .attr("opacity", function(d) {if(d.depth === 0) return 0; else return 1;});		//Hide first level.
 
+
+
+	//Use rectangles for first level.
+	nodeEnter.filter(function(d) {
+		if(d.depth === 1) return true;
+		else return false;
+	})
+	.append("rect")
+		.attr("width", 26)
+		.attr("height", 26)
+		.attr("x", -10)
+		.attr("y", -14)
+		.attr("fill", function(d) {return colors[d.name]})
+		//.style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; })
+		.attr("opacity", function(d) {if(d.depth === 0) return 0; else return 1;});		//Hide first level.
+
+
+
   nodeEnter.append("text")
-      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+      //.attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+	  	.attr("x", function(d) { if(d.depth === 1) return -25; else return d.children || d._children ? -10 : 10; })
       .attr("dy", ".35em")
       .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-      .text(function(d) { return d.name; })
+      .attr('class', d => d.name.length < 18 ? 'fulltext' : 'shorttext')
+      .text(function(d) {
+        return d.name.length < 40 ? d.name : d.name.slice(0, 39);
+      })
       .style("fill-opacity", 1e-6)
-	  .attr("opacity", function(d) {if(d.depth === 0) return 0; else return 1;});		//Hide first level.
+	  	.attr("opacity", function(d) {if(d.depth === 0) return 0; else return 1;}) //Hide first level.
+      .on('mouseover', function(d) {
+        if(d.name.length > 40) {
+          d3.select(this).text(d => d.name);
+        }
+      })
+      .on('mouseout', function(d) {
+        d3.select(this).text(d => d.name.slice(0, 39));
+      });
 
   // Transition nodes to their new position.
   var nodeUpdate = node.transition()
@@ -99,8 +288,9 @@ function update(source) {
       .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
   nodeUpdate.select("circle")
-      .attr("r", 4.5)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+      .attr("r", function(d) {if(d.name === searchText) return 9; else return 4.5})
+      //.style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+      //.style("fill", function(d) {return d._children ? colorMe(source.address) : "#FFF"});
 
   nodeUpdate.select("text")
       .style("fill-opacity", 1);
@@ -124,7 +314,8 @@ function update(source) {
   // Enter any new links at the parent's previous position.
   link.enter().insert("path", "g")
       .attr("class", "link")
-	  .attr("opacity", function(d) {if(d.source.depth === 0) return 0; else return 1;}) //Hides first level.
+	  	.style("stroke", function(d) { return colorMe(d.target.address) } )
+	  	.attr("opacity", function(d) { return d.source.depth == 0 ? 0 : 0.6 } ) //Hides first level.
       .attr("d", function(d) {
         var o = {x: source.x0, y: source.y0};
         return diagonal({source: o, target: o});
@@ -149,7 +340,14 @@ function update(source) {
     d.x0 = d.x;
     d.y0 = d.y;
   });
-  
+
+  /*d3.selectAll(".node")
+		.filter(function(a){
+			if(a.name === "Geographicals") return true;
+			else return false;
+		})
+		.attr("x", 0)
+		.attr("x0", 0);*/
 }
 
 
@@ -158,131 +356,91 @@ function update(source) {
 
 // Toggle children on click.
 function click(d) {
-  if (d.children) {
+  if(d._children) {
+    if(!d.children) d.children = d._children;
+    else {
+      for(child in d._children) {
+        d.children.push(d._children[child]);
+      }
+    }
+    d.children.sort(function(a, b) { return a.address.slice(a.address.length-3,a.address.length) > b.address.slice(b.address.length-3,b.address.length) ? 1 : -1 });
+    d._children = null;
+  } else if(d.children) {
     d._children = d.children;
     d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
   }
   update(d);
-  console.log("click function done");
+}
+
+
+
+
+function searchcsv(searchText) {
+	console.log("Searching for paths to: " + searchText);
+	var paths = [];
+	csvdata.forEach(function(d) {
+		if(d.name === searchText) {
+			paths.push(d.address);
+		}
+	});
+	return paths;
 }
 
 
 
 
 
-function expand(root) {
-	
-	var current = root;
-	
-	//Each address.
-	for(var i = 0; i < addresses.length; i++) {
-		var path = addresses[i].split(".");
-		var treeletter = path[0].slice(0,1);
-		
-		//Get the correct tree.
-		var tree;
-		for(var j = 0; j < root.children.length; j++) {
-			if(root.children[j].address === treeletter) tree = root.children[j];
-		}
-		
-		//The current node is the first node in a tree.
-		current = tree;
-		
-		console.log("Tree: " + treeletter);
-		console.log("Path: " + path);
-		
-		//The path to each node in the path.
-		//If the path is A.B.C then the nodes have paths
-		//A, A.B and A.B.C.
-		var nodesPaths = [];
-		for(var j = 0; j < path.length; j++) {
-			nodesPaths.push(path.slice(0, j+1).join("."));
-		}
-		
-		console.log("Paths: ");
-		console.log(nodesPaths);
-		
-		//Follow the path. At each node simulate a click.
-		for(var j = 0; j < nodesPaths.length; j++) {
-			console.log("Current node: " + current.name);
-			
-			//The path to the next node.
-			var pathToNextNode = nodesPaths[j];
-			
-			//console.log("Path to next node: " + nodesPaths[j]);
-			
-			if(current._children != null) {
-				//Hidden children. Look for a child with the correct path.
-				
-				for(var n = 0; n < current._children.length; n++) {
-					if(current._children[n].address === nodesPaths[j]) {
-						console.log("Next node: " + current._children[n].name);
-						//Save the node to click.
-						var nodeToClick = current;
-						//Change current node to the correct child.
-						current = current._children[n];
-						//Click the previously current node.
-						click(nodeToClick);
-						break;
-					}
-				}
-				
-			} else if(current.children != null) {
-				//Non hidden children. Look for a child with the correct path but don't click.
-				
-				for(var n = 0; n < current.children.length; n++) {
-					if(current.children[n].address === nodesPaths[j]) {
-						console.log("Next node (b): " + current.children[n].name);
-						//Change current node to the correct child.
-						current = current.children[n];
-						break;
-					}
-				}
-			}
-		}
-	}
+function search(string) {
+
+	//Get the paths to the nodes.
+	var paths = descToPaths[string.replace(/ /g, '').toLowerCase()];
+
+  if(paths != undefined) history.replaceState(null, "search", '?searchtext=' + encodeURIComponent(string));
+  else history.replaceState(null, "search", '?searchtext=');
+
+	console.log("Paths: ");
+	console.log(paths);
+
+	//searchTree(root, searchText);
+	//console.log(addresses);
+
+	//Expand the tree to the nodes.
+  root.children.forEach(collapse);
+  for(path in paths) {
+	   expand(root, paths[path]);
+  }
+  update(root);
+
+  //Apply filters.
+	d3.selectAll(".node")
+		.filter(function(a){
+			if(a.name === string) return true;
+			else return false;
+		})
+		.selectAll("circle")
+		.attr("r", 18)
+		.style("filter", function(d) {return "url(#glow)"});
 }
 
 
 
 
-
-function searchTree(node, searchText) {
-	
-	if(node.children != null) {
-		//console.log(node.children.length);
-		for(var i = 0; i < node.children.length; i++) {
-			searchTree(node.children[i]);
-			//console.log(node.children[i]);
-		}
-	} else if(node._children != null) {
-		for(var i = 0; i < node._children.length; i++) {
-			searchTree(node._children[i]);
-			//console.log(node.children[i]);
-		}
-	}
-	if(node.name === searchText) {
-		addresses.push(node.address);
-		//console.log(node);
-	}
-	return;
-}
-
-
-
-
-
-function search() {
-	
-	var searchText = $("#searchText");
-	
-	console.log("Search: " + searchText);
-	
-	searchTree(root, searchText);
-	console.log(addresses);
-	
-	expand(root);
+function colorMe(path) {
+	var treeName = path.slice(0,1);
+	if(treeName === "A") return colors["Anatomy"];
+	if(treeName === "B") return colors["Organisms"];
+	if(treeName === "C") return colors["Diseases"];
+	if(treeName === "D") return colors["Chemicals and Drugs"];
+	if(treeName === "E") return colors["Analytical, Diagnostic and Therapeutic Techniques, and Equipment"];
+	if(treeName === "F") return colors["Psychiatry and Psychology"];
+	if(treeName === "G") return colors["Phenomena and Processes"];
+	if(treeName === "H") return colors["Disciplines and Occupations"];
+	if(treeName === "I") return colors["Anthropology, Education, Sociology, and Social Phenomena"];
+	if(treeName === "J") return colors["Technology, Industry, and Agriculture"];
+	if(treeName === "K") return colors["Humanities"];
+	if(treeName === "L") return colors["Information Science"];
+	if(treeName === "M") return colors["Named Groups"];
+	if(treeName === "N") return colors["Health Care"];
+	if(treeName === "V") return colors["Publication Characteristics"];
+	if(treeName === "Z") return colors["Geographicals"];
 }
