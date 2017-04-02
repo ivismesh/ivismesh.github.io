a4b090ed2f69557c00ccb5e6b4e7360ee866511c
Lizumi <lizumi-chan@hotmail.com>
2017-04-02 22:04:07 +0200

    Added minimization of the right panel. Some animation work left.

---
+++ arbor/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('#minimize-btn').on('click', function() {
+    var element = document.getElementById('minimize-btn');
+    if(element.classList.contains('glyphicon-chevron-up')) {
+      minimize();
+    } else {
+      maximize();
+    }
+    
+  });
+});
+
+function minimize(){
+  $('#minimize-btn').removeClass('glyphicon-chevron-up');
+  $('#minimize-btn').addClass('glyphicon-chevron-down');
+  $('#right-panel').css("height", "24px");
+}
+
+function maximize(){
+  $('#minimize-btn').removeClass('glyphicon-chevron-down');
+  $('#minimize-btn').addClass('glyphicon-chevron-up');
+  $('#right-panel').css("height", "");
+}
